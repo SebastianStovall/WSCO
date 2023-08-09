b9a04eb5423fe37daa92b441f1689c1eabb6b01f
SebastianStovall <sebastianstovall@gmail.com>
2023-08-09 00:54:58 -0500

    commented back in seeding for init seed file

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -5,7 +5,7 @@ from .posts import seed_posts, undo_posts
 from .comments import seed_comments, undo_comments
 from .journals import seed_journals, undo_journals
 from .journalphotos import seed_journal_photos, undo_journal_photos
-# from .collections import seed_collections, undo_collections
+from .collections import seed_collections, undo_collections
 
 from app.models.db import db, environment, SCHEMA
 
@@ -22,7 +22,7 @@ def seed():
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        # undo_collections()
+        undo_collections()
         undo_journal_photos()
         undo_journals()
         undo_comments()
@@ -33,7 +33,7 @@ def seed():
     seed_comments()
     seed_journals()
     seed_journal_photos()
-    # seed_collections()
+    seed_collections()
     # Add other seed functions here
 
 
