05109b35e61d10bf562789f82b8b2b922fb58f61
SebastianStovall <sebastianstovall@gmail.com>
2023-08-08 23:46:12 -0500

    collections seeder fix 2

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -5,6 +5,7 @@ from .posts import seed_posts, undo_posts
 from .comments import seed_comments, undo_comments
 from .journals import seed_journals, undo_journals
 from .journalphotos import seed_journal_photos, undo_journal_photos
+from .collections import seed_collections, undo_collections
 
 from app.models.db import db, environment, SCHEMA
 
@@ -21,6 +22,7 @@ def seed():
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
+        undo_collections()
         undo_journal_photos()
         undo_journals()
         undo_comments()
@@ -31,12 +33,14 @@ def seed():
     seed_comments()
     seed_journals()
     seed_journal_photos()
+    seed_collections()
     # Add other seed functions here
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
+    undo_collections()
     undo_journal_photos()
     undo_journals()
     undo_comments()
